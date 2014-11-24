a8635d9cd8fe3afd27eb9c90b7aea02573e7ef28
mybeta <mybeta@users.noreply.github.com>
2014-11-24 09:33:22 +0800

    some change

--- src/com/nostra13/universalimageloader/cache/disc/impl/ext/DiskLruCache.java
+++ src/com/nostra13/universalimageloader/cache/disc/impl/ext/DiskLruCache.java
@@ -718,8 +718,7 @@ final class DiskLruCache implements Closeable {
 		}
 	}
 
-	private static String inputStreamToString(InputStream in)
-			throws IOException {
+	private static String inputStreamToString(InputStream in) throws IOException {
 		return Util.readFully(new InputStreamReader(in, Util.UTF_8));
 	}
 
